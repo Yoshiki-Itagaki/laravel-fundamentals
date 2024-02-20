6fbf3d81151d36d11da4f2b26df79494dd0a74da
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-02-20 22:00:30 +0900

    handled database migration

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for($i = 0; $i < 50; $i++ ){
+            DB::table("posts")->insert([
+                "title"=> Str::random(20),
+                "description" => Str::random(20),
+                "status"=> 1,
+                "publish_date" => date('Y-m-d'),
+                "user_id"=> 1,
+            ]);
+        }
+
+    }
+}
