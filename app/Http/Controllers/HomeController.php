1fd1fc4c369431c9b97fc5c7020c46a9ac77eaf0
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-02-24 22:45:04 +0900

    Used the insert statement

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,39 +12,24 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        // $blogs = [
-        //     [
-        //         'title' => 'Title one',
-        //         'body'=> 'this is a body text one',
-        //         'status' => 1
-        //     ],
-        //     [
-        //         'title' => 'Title two',
-        //         'body'=> 'this is a body text two',
-        //         'status' => 0
-        //     ],
-        //     [
-        //         'title' => 'Title three',
-        //         'body'=> 'this is a body text three',
-        //         'status' => 0
-        //     ],
-        //     [
-        //         'title' => 'Title four',
-        //         'body'=> 'this is a body text four',
-        //         'status' => 1
-        //     ]
-        // ];
-        // return DB::table('posts')->get();
-        // return DB::table('posts')->find(1);
-        // return DB::table('posts')->first();
-        // return DB::table('posts')->where('status','=', 1)->get();
-        // return DB::table('posts')->pluck('title', 'id');
-        // return DB::table('posts')->where('id', '=', 10)->get();
-        // return DB::table('posts')->where('id', '>', 10)->get();
-        // return DB::table('posts')->where('id', '<', 10)->get();
-        // return DB::table('posts')->where('status', 0)->get();
-        return DB::table('posts')->where('status', '!=', 0)->get();
+        DB::table('posts')->insert([
+            [
+                'title' => 'This is test data 2',
+                'description' => 'gagagagaga',
+                'status' => 1,
+                'publish_date' => date('Y-m-d'),
+                'user_id' => 1
+            ],
+            [
+                'title' => 'This is test data 3',
+                'description' => 'bababababa',
+                'status' => 1,
+                'publish_date' => date('Y-m-d'),
+                'user_id' => 1
+            ]
+        ]);
 
-        return view('home', compact('blogs'));
+        dd('success');
+
     }
 }
