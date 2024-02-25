0276fcf9f0d895a5bbb0e265acb41d60e16272ae
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-02-25 20:49:01 +0900

    Enabled soft-deleting in Model configuration and performed soft-deleting in the home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,29 +12,9 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-    //    $post = Post::create([
-    //         "title"=> 'this is from mass assignment',
-    //         'description' => 'this is decription for mass assignment',
-    //         'status' => 1,
-    //         'publish_date' => date('Y-m-d'),
-    //         'user_id' => 1,
-    //         'category_id' => 2,
-    //         'views' => 500
-    //    ]);
+        Post::where('id', 59)->delete();
 
-        // $post = Post::find(59)->update([
-        //     "title"=> 'The data has been updated!',
-        //     "description" => "Description for the updated data"
-        // ]);
-
-        $post = Post::where('status', 1)->where('id', '<', 10)->update([
-            "title"=> 'The data has been updated!',
-            "description" => "Description for the updated data",
-            "status" => 0
-        ]);
-
-        dd('Success!');
-
+        dd('Successful!');
 
     }
 }
