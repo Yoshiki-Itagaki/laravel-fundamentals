a13e0fbdbcfe7dd393d8e4515a073cd857d2121f
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-02-25 15:23:14 +0900

    Updating data with Eloquent Models

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,19 +12,17 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-       $post = new Post();
-
-       $post->title = 'new post 2024/02/25';
-       $post->description = 'description for the new post 2024/02/25';
-       $post->status = 1;
-       $post->publish_date = date('Y-m-d');
-       $post->user_id = 1;
-       $post->category_id = 2;
-       $post->views = 400;
+       $post = Post::find(57);
+       $post->title = 'new post 2024/02/25 which will be updated this time';
+       $post->description = 'description for the update post 2024/02/25, 3 p.m.';
 
        $post->save();
 
-       dd('Success!');
+       return $post;
+
+       //find() -> return a record
+       //get() -> return an array of a record(s)
+
 
     }
 }
