78a660531d7caf398268aa3f5989788ee0c44afc
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-02-25 15:27:59 +0900

    Deleting data with Eloquent Models

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,17 +12,10 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-       $post = Post::find(57);
-       $post->title = 'new post 2024/02/25 which will be updated this time';
-       $post->description = 'description for the update post 2024/02/25, 3 p.m.';
+       Post::where('id', 58)->delete();
+    //    Post::findOrFail('id', 58)->delete();
 
-       $post->save();
-
-       return $post;
-
-       //find() -> return a record
-       //get() -> return an array of a record(s)
-
+       dd('success');
 
     }
 }
