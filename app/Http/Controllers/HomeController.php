a22516969ee2899229607dc012d28e107d3f0605
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-02-25 14:10:28 +0900

    Used the aggregate statements

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,114 +12,18 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        // #1 join query without the select query
+        //# Aggregate Statements
 
-        // return DB::table('posts')->join('categories', 'posts.category_id', '=', 'categories.id')->get();
-
-        // {
-        //     "id": 1,
-        //     "title": "PmhivFTPYNXh47fplFPf",
-        //     "description": "A8ZZkHHUgUurOTGgTIqb",
-        //     "status": 1,
-        //     "created_at": null,
-        //     "updated_at": null,
-        //     "publish_date": "2024-02-20",
-        //     "user_id": 1,
-        //     "category_id": 1,
-        //     "name": "News"
-        // },
-        // {
-        //     "id": 2,
-        //     "title": "The data is updated again!",
-        //     "description": "This is a whole new description!",
-        //     "status": 1,
-        //     "created_at": null,
-        //     "updated_at": null,
-        //     "publish_date": "2024-02-24",
-        //     "user_id": 1,
-        //     "category_id": 2,
-        //     "name": "Tech"
-        // },
-        // {
-        //     "id": 3,
-        //     "title": "Updated the data!",
-        //     "description": "Testing out the insert statement!",
-        //     "status": 1,
-        //     "created_at": null,
-        //     "updated_at": null,
-        //     "publish_date": "2024-02-24",
-        //     "user_id": 1,
-        //     "category_id": 3,
-        //     "name": "Education"
-        // }
-
-
-
-        // #2 join query with the select query for the table 'posts'
-
-        // return DB::table('posts')->join('categories', 'posts.category_id', '=', 'categories.id')
-        // ->select('posts.*')
-        // ->get();
-
-        // {
-        //     "id": 52,
-        //     "title": "PmhivFTPYNXh47fplFPf",
-        //     "description": "A8ZZkHHUgUurOTGgTIqb",
-        //     "status": 1,
-        //     "created_at": null,
-        //     "updated_at": null,
-        //     "publish_date": "2024-02-20",
-        //     "user_id": 1,
-        //     "category_id": 1
-        // },
-        // {
-        //     "id": 54,
-        //     "title": "The data is updated again!",
-        //     "description": "This is a whole new description!",
-        //     "status": 1,
-        //     "created_at": null,
-        //     "updated_at": null,
-        //     "publish_date": "2024-02-24",
-        //     "user_id": 1,
-        //     "category_id": 2
-        // },
-        // {
-        //     "id": 55,
-        //     "title": "Updated the data!",
-        //     "description": "Testing out the insert statement!",
-        //     "status": 1,
-        //     "created_at": null,
-        //     "updated_at": null,
-        //     "publish_date": "2024-02-24",
-        //     "user_id": 1,
-        //     "category_id": 3
-        // }
-
-
-         // #3 join query with the select query for the table 'categories'
-
-         return DB::table('posts')->join('categories', 'posts.category_id', '=', 'categories.id')
-         ->select('categories.*')
-         ->get();
-
-        //  {
-        //     "id": 1,
-        //     "name": "News",
-        //     "created_at": null,
-        //     "updated_at": null
-        // },
-        // {
-        //     "id": 2,
-        //     "name": "Tech",
-        //     "created_at": null,
-        //     "updated_at": null
-        // },
-        // {
-        //     "id": 3,
-        //     "name": "Education",
-        //     "created_at": null,
-        //     "updated_at": null
-        // }
+        //count
+        // return DB::table('posts')->count();
+        //max
+        // return DB::table('posts')->max('views');
+        //min
+        // return DB::table('posts')->min('views');
+        //avg
+        // return DB::table('posts')->avg('views');
+        //sum
+        return DB::table('posts')->sum('views');
 
 
     }
